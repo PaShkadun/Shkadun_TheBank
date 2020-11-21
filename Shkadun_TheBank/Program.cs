4da25bd8de63eae9baa16f6acd5a875fb14a9179
rbisb <rbisb@DESKTOP-MM26GKA>
2020-11-21 17:29:40 +0300

    Добавьте файлы проекта.

---
+++ Shkadun_TheBank/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shkadun_TheBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
